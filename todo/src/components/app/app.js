6f1684dfce3aa678331bf2307c28cd4e364015db
Lev Ufimtsev <raykind@list.ru>
2020-01-16 10:31:58 +0500

    5

---
+++ todo/src/components/app/app.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import AppHeader from "../app-header";
+import SearchPanel from '../search-panel';
+import TodoList from '../todo-list';
+import ItemStatusFilter from "../item-status-filter";
+
+import './app.css';
+
+export default class App extends React.Component {
+    state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a Lunch', important: false, id: 3}
+        ]
+    };
+
+    deleteItem = (id) => {
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+            todoData.splice(idx, 1);
+
+            return {
+                todoData: todoData
+            }
+        })
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList
+                    todos={this.state.todoData}
+                    onDeleted={this.deleteItem}/>
+            </div>
+        );
+    }
+};
--- todo/src/components/todo-list/todo-list.js
+++ todo/src/components/todo-list/todo-list.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import './todo-list.css';
 
-import TodoListItem from "./todo-list-item";
+import TodoListItem from "../todo-list-item";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
 
     const elements = todos.map((item) => {
         const {id, ...itemData} = item;
 
         return (
             <li key={id} className="list-group-item">
-                <TodoListItem {...itemData} />
+                <TodoListItem
+                    {...itemData}
+                    onDeleted={() => onDeleted(id)}/>
             </li>
         )
     });
